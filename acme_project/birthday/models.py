3563b16e96bd1293843c15af98ca38c1693e97cc
Denis Savvateev <densavvateev@yandex.ru>
2023-11-01 05:33:46 +0300

    Add all types of validation

--- acme_project/birthday/models.py
+++ acme_project/birthday/models.py
@@ -1,9 +1,27 @@
+# birthday/models.py
 from django.db import models
+
+# Импортируется функция-валидатор.
+from .validators import real_age
 
 
 class Birthday(models.Model):
-    first_name = models.CharField('Имя', max_length=20)
+    first_name = models.CharField(max_length=20, verbose_name='Имя')
     last_name = models.CharField(
-        'Фамилия', blank=True, help_text='Необязательное поле', max_length=20
+        max_length=20,
+        help_text='Необязательное поле',
+        blank=True,
+        verbose_name='Фамилия'
     )
-    birthday = models.DateField('Дата рождения')
+    # Валидатор указывается в описании поля.
+    birthday = models.DateField('Дата рождения', validators=(real_age,))
+
+    class Meta():
+        verbose_name = 'день рождения'
+        verbose_name_plural = 'Дни рождения'
+        constraints = (
+            models.UniqueConstraint(
+                fields=('first_name', 'last_name', 'birthday'),
+                name='Unique person constraint',
+            ),
+        )
