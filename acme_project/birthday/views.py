af0f77283abaa5d4ff8d395ae1e009e5128b78a3
Денис Савватеев <densavvateev@yandex.ru>
2023-11-03 00:04:28 +0300

    Refactor project with CBV-classes

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,79 +1,83 @@
 # birthday/views.py
-from django.shortcuts import get_object_or_404, redirect, render
-from django.core.paginator import Paginator
+# Для создания CBV-объектов
+from django.views.generic import (
+    CreateView, DeleteView, DetailView, ListView, UpdateView
+)
+# reverse_lazy() срабатывает только при непосредственном обращении
+# к CBV во время работы веб-сервера, а не на этапе запуска проекта,
+# когда импортируются все классы. В момент запуска проекта карта
+# маршрутов может быть ещё не сформирована, и использование обычного
+# reverse() вызовет ошибку
+from django.urls import reverse_lazy
 
 from .forms import BirthdayForm
-# Импортируем из utils.py функцию для подсчёта дней.
-from .utils import calculate_birthday_countdown
 # Импортируем модель дней рождения.
 from .models import Birthday
+# Импортируем из utils.py функцию для подсчёта дней.
+from .utils import calculate_birthday_countdown
 
 
-# Добавим опциональный параметр pk.
-def birthday(request, pk=None):
-    if pk is not None:
-        # Получаем объект модели или выбрасываем 404 ошибку.
-        instance = get_object_or_404(Birthday, pk=pk)
-    # Если в запросе не указан pk
-    # (если получен запрос к странице создания записи):
-    else:
-        # Связывать форму с объектом не нужно, установим значение None.
-        instance = None
-    # Передаём в форму либо данные из запроса, либо None.
-    # В случае редактирования прикрепляем объект модели.
-    form = BirthdayForm(
-        request.POST or None,
-        # Файлы, переданные в запросе, указываются отдельно.
-        files=request.FILES or None,
-        instance=instance
-    )
-    # Создаём словарь контекста сразу после инициализации формы.
-    context = {'form': form}
-    # Если форма валидна...
-    if form.is_valid():
-        # Добавим строчку с вызовом метода save(), для записи данных
-        # формы в базу данных
-        form.save()
-        # ...вызовем функцию подсчёта дней:
-        birthday_countdown = calculate_birthday_countdown(
-            # ...и передаём в неё дату из словаря cleaned_data.
-            form.cleaned_data['birthday']
+# Наследуем класс от встроенного ListView:
+class BirthdayListView(ListView):
+    # Указываем модель, с которой работает CBV...
+    model = Birthday
+    # ...сортировку, которая будет применена при выводе списка объектов:
+    ordering = 'id'
+    # ...и даже настройки пагинации:
+    paginate_by = 10
+
+
+# Создаём миксины.
+# И убираем, за ненадобностью... Оставляю, как пример.
+# class BirthdayMixin:
+    # Указываем модель, с которой работает CBV...
+#    model = Birthday
+    # Указываем namespace:name страницы, куда будет перенаправлен пользователь
+    # после создания объекта:
+#    success_url = reverse_lazy('birthday:list')
+
+# Убираем в связи с переименованием шаблона birthday.html' в
+# ожидаемое классом имя.
+# class BirthdayFormMixin:
+    # Сопутствующий класс сам может создать форму на основе модели!
+    # Нет необходимости отдельно создавать форму через ModelForm.
+    # Указываем поля, которые должны быть в форме:
+    # fields = '__all__'
+    # Но виджетом-календарь и контроль имён у нас в отдельной форме
+    # Указываем вместо полей имя формы:
+#    form_class = BirthdayForm
+    # По логике классов CBV, имя шаблона должно быть birthday_form.html
+    # Поэтому мы явным образом указываем шаблон:
+#    template_name = 'birthday/birthday.html'
+
+
+# Добавляем миксин первым по списку родительских классов.
+# Убрали миксин за ненадобностью
+class BirthdayCreateView(CreateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayUpdateView(UpdateView):
+    model = Birthday
+    form_class = BirthdayForm
+
+
+class BirthdayDeleteView(DeleteView):
+    model = Birthday
+    success_url = reverse_lazy('birthday:list')
+
+
+class BirthdayDetailView(DetailView):
+    model = Birthday
+
+    def get_context_data(self, **kwargs):
+        # Получаем словарь контекста:
+        context = super().get_context_data(**kwargs)
+        # Добавляем в словарь новый ключ:
+        context['birthday_countdown'] = calculate_birthday_countdown(
+            # Дату рождения берём из объекта в словаре context:
+            self.object.birthday
         )
-        # Обновляем словарь контекста: добавляем в него новый элемент.
-        context.update({'birthday_countdown': birthday_countdown})
-    return render(request, 'birthday/birthday.html', context)
-
-
-def delete_birthday(request, pk):
-    # Получаем объект модели или выбрасываем 404 ошибку.
-    instance = get_object_or_404(Birthday, pk=pk)
-    # В форму передаём только объект модели;
-    # передавать в форму параметры запроса не нужно.
-    form = BirthdayForm(instance=instance)
-    context = {'form': form}
-    # Если был получен POST-запрос...
-    if request.method == 'POST':
-        # ...удаляем объект:
-        instance.delete()
-        # ...и переадресовываем пользователя на страницу со списком записей.
-        return redirect('birthday:list')
-    # Если был получен GET-запрос — отображаем форму.
-    return render(request, 'birthday/birthday.html', context)
-
-
-def birthday_list(request):
-    # Получаем список всех объектов с сортировкой по id.
-    birthdays = Birthday.objects.order_by('id')
-    # Создаём объект пагинатора с количеством 10 записей на страницу.
-    paginator = Paginator(birthdays, 10)
-
-    # Получаем из запроса значение параметра page.
-    page_number = request.GET.get('page')
-    # Получаем запрошенную страницу пагинатора.
-    # Если параметра page нет в запросе или его значение не приводится к числу,
-    # вернётся первая страница.
-    page_obj = paginator.get_page(page_number)
-    # Вместо полного списка объектов передаём в контекст
-    # объект страницы пагинатора
-    context = {'page_obj': page_obj}
-    return render(request, 'birthday/birthday_list.html', context)
+        # Возвращаем словарь контекста.
+        return context
